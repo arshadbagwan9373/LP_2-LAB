c9af01ef93eae138624c39f179cd0eaf901aba12
Arshad Bagwan <arshadbagwan9373@gmail.com>
2026-05-17 23:25:04 +0530

    Google App Engine

---
+++ demo.py
@@ -0,0 +1,14 @@
+print("\n\n-------------------- // -------------------------------")
+print("----------- GOOGLE CLOUD ENGINE PRACTICAL ---------------")
+print("---------------------------------------------------------\n")
+print("This Code is Written in python and stored at GitHub ")
+print("Hello From GitHub.............")
+name = input("Enter your Name : ")
+print("Hello "+name + ".... Welcome To Cloud Engine")
+
+
+for i in range(0,15):
+  for j in range(0,i+1):
+      print("*",end="")
+  print("")
+print("---------------------- // -------------------------------")
